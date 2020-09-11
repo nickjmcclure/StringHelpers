42416932101b840f4d6ebd7008ba0ac1ed197b48
Nick McClure <nickmcclure@outlook.com>
2020-09-10 22:59:18 -0400

    Add project files.

---
+++ StringHelpers/Encode.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Net;
+
+namespace Nacho.Helpers.Encode
+{
+    public static class Encode
+    {
+        public static string Base64Encode(this string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(this string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+
+        public static string UrlEncode(this string instr)
+        {
+            return WebUtility.UrlEncode(instr);
+        }
+
+        public static string UrlDecode(this string instr)
+        {
+            return WebUtility.UrlDecode(instr);
+        }
+    }
+}
---
+++ StringHelpers/Hash.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Nacho.Helpers.Hash
+{
+    public static class Hash
+    {
+        public static string getMD5Hash(string instr)
+        {
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(instr));
+            StringBuilder sBuilder = new StringBuilder();
+
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        public static string toMD5Hash(this string instr)
+        {
+            return getMD5Hash(instr);
+        }
+    }
+}
